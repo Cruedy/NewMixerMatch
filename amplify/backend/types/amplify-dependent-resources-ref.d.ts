b06ca6e8f8f0ab10f3da0351d29b3192637e2bc2
cruedy <cruedy@hmc.edu>
2024-09-04 15:58:54 -0700

    still adding database

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mixermatch": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
